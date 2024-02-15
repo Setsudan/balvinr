4da9250ce386b2dcf325b47b407cf53c48f99352
Setsudashi <cryptiareta@gmail.com>
2024-02-15 12:21:08 +0100

    return

--- composables/useStorage.ts
+++ composables/useStorage.ts
@@ -29,4 +29,6 @@ export default function useStorage() {
         const storageRef = fbref(storage, `images/${file.name}`);
         await uploadBytes(storageRef, file);
     }
+
+    return { addProfilePicture, getProfilePicture, uploadImage };
 }
